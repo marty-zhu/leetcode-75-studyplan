0149ea33edb81e619884533e7a7a8920b281e4b4
marty-zhu <marty.zhu@gmail.com>
2022-08-02 11:16:59 -0700

    initial commit

---
+++ tests/test_n724_pivot_index.py
@@ -0,0 +1,16 @@
+import pytest
+from solutions.n724_pivot_index import pivot_index
+
+class TestPivotIndex:
+
+    @pytest.mark.xfail("function not written yet as per TDD.")
+    def test_none_edge_index():
+        pass
+
+    @pytest.mark.xfail("function not written yet as per TDD.")
+    def test_left_edge_index():
+        pass
+
+    @pytest.mark.xfail("function not written yet as per TDD.")
+    def test_right_edge_index():
+        pass
